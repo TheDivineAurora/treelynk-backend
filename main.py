31fb514b644ad257bc45b878cf1b8f97c37c713e
Karthik Reddy <118504534+TheDivineAurora@users.noreply.github.com>
2025-08-24 11:43:31 +0000

    minor changes in main.py

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from core.database import engine, Base
+from sqlalchemy import text
+from routers import auth
+Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Treelynk API")
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        with engine.connect() as conn:
+            conn.execute(text("SELECT 1"))
+            print("Connection to Database is Successful")
+    except Exception as e:
+        print("Database Connection Failed: ", e)
+    yield
+
+app = FastAPI(lifespan = lifespan)
+
+app.include_router(auth.router, prefix = "/auth", tags = ["Auth"])
 
 @app.get("/")
 def root():
     return {"message": "Treelynk backend is running"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host = "0.0.0.0", port = 8000, reload = True)
